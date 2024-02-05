81d4fa04feb4cc199cdb6acc9e0598f434e61d30
amol1415 <amolmaharnavar90@gmail.com>
2024-02-05 14:43:52 +0530

    changes add

---
+++ index.js
@@ -0,0 +1,9 @@
+let x = "amol";
+console.log(x);
+console.log("bhairav");
+console.log("bhairav");
+console.log("bhairav");
+console.log("bhairav");
+console.log("bhairav");
+console.log("bhairav");
+console.log("bhairav");
